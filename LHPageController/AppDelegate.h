64b73048780ba39ac5c4d8a868a8dbd0f0127f85
陈汉鑫 <alex@mbp-dad.local>
2016-07-29 10:23:00 +0800

    Initial Commit

---
+++ LHPageController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHPageController
+//
+//  Created by 陈汉鑫 on 16/7/29.
+//  Copyright © 2016年 陈汉鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHPageController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHPageController
+//
+//  Created by 陈汉鑫 on 16/7/29.
+//  Copyright © 2016年 陈汉鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
